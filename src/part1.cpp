e3bc170447b60a4ae20dbebda76c679cb0604532
TheCDeveloper <230489966+TheCDeveloper@users.noreply.github.com>
2026-02-17 21:34:24 +0530

    Restructure code for readability
    Improves readability by using a column limit of 100 characters, and
    combining `println` and `sleep` functions calls to an inlined function.

--- src/part1.cpp
+++ src/part1.cpp
@@ -4,69 +4,79 @@
 
 void part1() 
 {
-    println("-------- PART 1 --------");
-    sleep(2);
-    println("It's a weekend and you decide to do something... fun. What will you do?");
-    sleep(4);
+    showMessage("-------- PART 1 --------", 2);
+    showMessage("It's a weekend and you decide to do something... fun. What will you do?", 4);
 
-    println("A. Go to the mall with your friends.");
-    sleep(1);
-    println("B. Watch a TV Show/Anime.");
-    sleep(1);
-    println("C. Hangout with your friends at your house.");
-    sleep(1);
+    showMessage("A. Go to the mall with your friends.", 1);
+    showMessage("B. Watch a TV Show/Anime.", 1);
+    showMessage("C. Hangout with your friends at your house.", 1);
+
 
     char decision = getResponse("> ", { 'a', 'b', 'c' });
     switch (decision) {
         case 'a':
-            println("You call up your friends, and after a while, you guys meet up at the mall.");
-            sleep(3);
-            println("As you all are exploring the mall, you suddenly start seeing hallucinations of a sort of long haired creature crying.");
-            sleep(5);
-            println("You tell your friends about this and they just ignore it as they think you're just playing with them.");
-            sleep(4);
-            println("After having fun for about 1 and a half hour, you and your friends decide to head back.");
-            sleep(4);
-            println("However, as you're leaving the mall, you see that creature again and this time it's looking at you and seems to be calling you.");
-            sleep(6);
-            println("Your friends have all gone away and now only you are left at the parking lot.");
-            sleep(4);
+            showMessage("You call up your friends, and after a while, "
+                        "you guys meet up at the mall.", 3);
+
+            showMessage("As you all are exploring the mall, you suddenly start seeing "
+                        "hallucinations of a sort of long haired creature crying.", 5);
+
+            showMessage("You tell your friends about this and they just ignore it "
+                        "as they think you're just playing with them.", 4);
+
+            showMessage("After having fun for about 1 and a half hour, you and your friends "
+                        "decide to head back.", 4);
+
+            showMessage("However, as you're leaving the mall, you see that creature again "
+                        "and this time it's looking at you and seems to be calling you.", 6);
+
+            showMessage("Your friends have all gone away and now only you are left at "
+                        "the parking lot.", 4);
 
             part2a();
             break;
 
+
         case 'b':
-            println("You decide to stay home to rest and watch a show you've been wanting to watch for a long time.");
-            sleep(4);
-            println("As you're watching, you sense a weird presence, like if someone was watching you.");
-            sleep(4);
-            println("You look around yourself and you suddenly see a long haired creature crying but as you look back at it, it's gone.");
-            sleep(5);
-            println("You get worried and call the police, and wait for a while until they arrive.");
-            sleep(4);
-            println("After around 15 minutes, the police ring the door bell and enter your apartment.");
-            sleep(4);
-            println("They then investigate your entire house and check the camera footage, but, there's nothing weird to be found.");
-            sleep(5);
-            println("They think you're just pranking them and leave your apartment.");
-            sleep(4);
+            showMessage("You decide to stay home to rest and watch a show you've been "
+                        "wanting to watch for a long time.", 4);
+
+            showMessage("As you're watching, you sense a weird presence, like if "
+                        "someone was watching you.", 4);
+
+            showMessage("You look around yourself and you suddenly see a "
+                        "long haired creature crying but as you look back at it, it's gone.", 5);
+
+            showMessage("You get worried and call the police, and wait for a "
+                        "while until they arrive.", 4);
+
+            showMessage("After around 15 minutes, the police ring the door bell and "
+                        "enter your apartment.", 4);
+
+            showMessage("They then investigate your entire house and check the camera footage, "
+                        "but, there's nothing weird to be found.", 5);
+
+            showMessage("They think you're just pranking them and leave your apartment.", 4);
 
             part2b();
             break;
 
+
         case 'c':
-            println("You decide to call your friends, and 3 of them agree to come.");
-            sleep(4);
-            println("After about 30 minutes, they all show up at your house.");
-            sleep(3);
-            println("You guys decide to start off by playing a game of UNO.");
-            sleep(4);
-            println("You all are having fun playing the card game, however, you suddenly start to see a long haired creature crying behind your friend.");
-            sleep(6);
-            println("You tell your friends about it, but they don't see anything off and just assume you're pranking them.");
-            sleep(5);
-            println("You get worried, but think you might just be hallucinating and continue playing the game.");
-            sleep(5);
+            showMessage("You decide to call your friends, and 3 of them agree to come.", 4);
+
+            showMessage("After about 30 minutes, they all show up at your house.", 3);
+
+            showMessage("You guys decide to start off by playing a game of UNO.", 4);
+
+            showMessage("You all are having fun playing the card game, however, you suddenly "
+                        "start to see a long haired creature crying behind your friend.", 6);
+
+            showMessage("You tell your friends about it, but they don't see anything off "
+                        "and just assume you're pranking them.", 5);
+
+            showMessage("You get worried, but think you might just be hallucinating "
+                        "and continue playing the game.", 5);
 
             part2c();
             break;
