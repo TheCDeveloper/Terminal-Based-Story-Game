cd12048cdbf5fe5d0747c7bb3cac5dd4d3a180c7
Story User <story@example.com>
2026-02-17 15:00:11 +0530

    Added story for P1->A->P2->B

--- part2.cpp
+++ part2.cpp
@@ -21,18 +21,30 @@ void part2a()
             std::cout << "You decide to just ignore the creature you saw and calm your mind by going home.\n";
             sleep(4);
             std::cout << "Once you reach your home, you get a snack and decide to chill.\n";
-            sleep(3);
+            sleep(4);
             std::cout << "You're watching something on your TV, when suddenly, you just get sudden panic attacks of sorts.\n";
             sleep(5);
             std::cout << "You see that creature as a distorted image of sorts and feel very dizzy.\n";
             sleep(4);
-            std::cout << "You decide to just sleep to cool your mind\n";
-            sleep(3);
+            std::cout << "You decide to just sleep to cool your mind.\n";
+            sleep(4);
             break;                        
         }
         else if (decision == "B" || decision == "b")
         {
-
+            std::cout << "You decide to walk towards the creature, to see what it wants.\n";
+            sleep(4);
+            std::cout << "As you get closer to the creature, the world seems to get more distorted.\n";
+            sleep(4);
+            std::cout << "You feel dizzy aswell, but decide to keep walking.\n";
+            sleep(4);
+            std::cout << "As you are about to make contact with the creature...\n";
+            sleep(4);
+            std::cout << "You feel a very heavy force and feel like you got blown away.\n";
+            sleep(4);
+            std::cout << "When you open your eyes, you see you're at your house, still wondering what happened.\n";
+            sleep(5);
+            break;
         }
         else if (decision == "C" || decision == "c")
         {
