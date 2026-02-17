df1538d06b1019cf1c5e2fcc68a611fbfa3cd555
Story User <story@example.com>
2026-02-17 14:44:00 +0530

    Added story for P1->A->P2->A

--- part2.cpp
+++ part2.cpp
@@ -18,7 +18,17 @@ void part2a()
         std::cin >> decision;
         if (decision == "A" || decision == "a")
         {
-
+            std::cout << "You decide to just ignore the creature you saw and calm your mind by going home.\n";
+            sleep(4);
+            std::cout << "Once you reach your home, you get a snack and decide to chill.\n";
+            sleep(3);
+            std::cout << "You're watching something on your TV, when suddenly, you just get sudden panic attacks of sorts.\n";
+            sleep(5);
+            std::cout << "You see that creature as a distorted image of sorts and feel very dizzy.\n";
+            sleep(4);
+            std::cout << "You decide to just sleep to cool your mind\n";
+            sleep(3);
+            break;                        
         }
         else if (decision == "B" || decision == "b")
         {
