64ae22c9ada18b2a923e11ebce6de63eada86d6e
TheCDeveloper <230489966+TheCDeveloper@users.noreply.github.com>
2026-02-20 16:15:50 +0530

    Add placeholder
    Adds placeholder messages to make it consistent and easy to modify
    across the codebase.

--- src/part3.cpp
+++ src/part3.cpp
@@ -1,47 +1,272 @@
 #include "parts.h"
 #include "util.h"
 
+
 void part3aa()
 {
+    showMessage("What will you do now?", 2);
+    showMessage("A. ...", 1);
+    showMessage("B. ...", 1);
+    showMessage("C. ...", 1);
 
+
+    char decision = getResponse("> ", { 'a', 'b', 'c' });
+    switch (decision) {
+        case 'a':
+            showMessage("...", 1);
+
+            break;
+
+
+        case 'b':
+            showMessage("...", 1);
+
+            break;
+
+
+        case 'c':
+            showMessage("...", 1);
+
+            break;
+    }
 }
+
 
 void part3ab()
 {
+    showMessage("What will you do now?", 2);
+    showMessage("A. ...", 1);
+    showMessage("B. ...", 1);
+    showMessage("C. ...", 1);
 
+
+    char decision = getResponse("> ", { 'a', 'b', 'c' });
+    switch (decision) {
+        case 'a':
+            showMessage("...", 1);
+
+            break;
+
+
+        case 'b':
+            showMessage("...", 1);
+
+            break;
+
+
+        case 'c':
+            showMessage("...", 1);
+
+            break;
+    }
 }
+
 
 void part3ac()
 {
-	
+    showMessage("What will you do now?", 2);
+    showMessage("A. ...", 1);
+    showMessage("B. ...", 1);
+    showMessage("C. ...", 1);
+
+
+    char decision = getResponse("> ", { 'a', 'b', 'c' });
+    switch (decision) {
+        case 'a':
+            showMessage("...", 1);
+
+            break;
+
+
+        case 'b':
+            showMessage("...", 1);
+
+            break;
+
+
+        case 'c':
+            showMessage("...", 1);
+
+            break;
+    }
 }
+
 
 void part3ba()
 {
+    showMessage("What will you do now?", 2);
+    showMessage("A. ...", 1);
+    showMessage("B. ...", 1);
+    showMessage("C. ...", 1);
 
+
+    char decision = getResponse("> ", { 'a', 'b', 'c' });
+    switch (decision) {
+        case 'a':
+            showMessage("...", 1);
+
+            break;
+
+
+        case 'b':
+            showMessage("...", 1);
+
+            break;
+
+
+        case 'c':
+            showMessage("...", 1);
+
+            break;
+    }
 }
+
 
 void part3bb()
 {
+    showMessage("What will you do now?", 2);
+    showMessage("A. ...", 1);
+    showMessage("B. ...", 1);
+    showMessage("C. ...", 1);
 
+
+    char decision = getResponse("> ", { 'a', 'b', 'c' });
+    switch (decision) {
+        case 'a':
+            showMessage("...", 1);
+
+            break;
+
+
+        case 'b':
+            showMessage("...", 1);
+
+            break;
+
+
+        case 'c':
+            showMessage("...", 1);
+
+            break;
+    }
 }
+
 
 void part3bc()
 {
+    showMessage("What will you do now?", 2);
+    showMessage("A. ...", 1);
+    showMessage("B. ...", 1);
+    showMessage("C. ...", 1);
 
+
+    char decision = getResponse("> ", { 'a', 'b', 'c' });
+    switch (decision) {
+        case 'a':
+            showMessage("...", 1);
+
+            break;
+
+
+        case 'b':
+            showMessage("...", 1);
+
+            break;
+
+
+        case 'c':
+            showMessage("...", 1);
+
+            break;
+    }
 }
+
 
 void part3ca()
 {
+    showMessage("What will you do now?", 2);
+    showMessage("A. ...", 1);
+    showMessage("B. ...", 1);
+    showMessage("C. ...", 1);
 
+
+    char decision = getResponse("> ", { 'a', 'b', 'c' });
+    switch (decision) {
+        case 'a':
+            showMessage("...", 1);
+
+            break;
+
+
+        case 'b':
+            showMessage("...", 1);
+
+            break;
+
+
+        case 'c':
+            showMessage("...", 1);
+
+            break;
+    }
 }
+
 
 void part3cb()
 {
+    showMessage("What will you do now?", 2);
+    showMessage("A. ...", 1);
+    showMessage("B. ...", 1);
+    showMessage("C. ...", 1);
 
+
+    char decision = getResponse("> ", { 'a', 'b', 'c' });
+    switch (decision) {
+        case 'a':
+            showMessage("...", 1);
+
+            break;
+
+
+        case 'b':
+            showMessage("...", 1);
+
+            break;
+
+
+        case 'c':
+            showMessage("...", 1);
+
+            break;
+    }
 }
+
 
 void part3cc()
 {
+    showMessage("What will you do now?", 2);
+    showMessage("A. ...", 1);
+    showMessage("B. ...", 1);
+    showMessage("C. ...", 1);
 
+
+    char decision = getResponse("> ", { 'a', 'b', 'c' });
+    switch (decision) {
+        case 'a':
+            showMessage("...", 1);
+
+            break;
+
+
+        case 'b':
+            showMessage("...", 1);
+
+            break;
+
+
+        case 'c':
+            showMessage("...", 1);
+
+            break;
+    }
 }
