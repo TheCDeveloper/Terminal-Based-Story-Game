c04ad8e922ea9c3088b7fce0c14a225a41cdd381
Story777 <250651167+Story777@users.noreply.github.com>
2026-02-20 16:00:08 +0530

    Add Part 3
    Implemented function declarations for Part 3

---
+++ src/part3.cpp
@@ -0,0 +1,47 @@
+#include "parts.h"
+#include "util.h"
+
+void part3aa()
+{
+
+}
+
+void part3ab()
+{
+
+}
+
+void part3ac()
+{
+	
+}
+
+void part3ba()
+{
+
+}
+
+void part3bb()
+{
+
+}
+
+void part3bc()
+{
+
+}
+
+void part3ca()
+{
+
+}
+
+void part3cb()
+{
+
+}
+
+void part3cc()
+{
+
+}
--- src/parts.h
+++ src/parts.h
@@ -5,3 +5,13 @@ void part1();
 void part2a();
 void part2b();
 void part2c();
+
+void part3aa();
+void part3ab();
+void part3ac();
+void part3ba();
+void part3bb();
+void part3bc();
+void part3ca();
+void part3cb();
+void part3cc();
